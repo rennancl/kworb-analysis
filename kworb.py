ebf20a3e2fdf0536be313b4cbf013208242d9691
Rennan Cordeiro <rennancl@gmail>
2018-04-28 18:50:07 -0300

    finalizado criacao do .csv

--- kworb.py
+++ kworb.py
@@ -25,10 +25,22 @@ for i in range(delta.days + 1):
     table_data = [[cell.text.encode(soup.original_encoding) for cell in row("td")]
         for row in table("tr")]
     headers = [th.text.encode(soup.original_encoding) for th in table.select("tr th")]
-    
+    splited = headers[1].split(' and ')   
+    headers[3:len(headers)+1] = headers[2:-1]
+    headers[1] = splited[0]
+    headers[2] = splited[1]
+
+
     # Produz arquivo .csv
     with open("arquivos/" + data + ".out.csv", "w") as f:
         wr = csv.writer(f)
         wr.writerow(headers)
-        wr.writerows(table_data)
+        i = 1
+        while i < len(table_data):
+            splited = table_data[i][1].split('-')   
+            table_data[i][3:len(row)+1] = table_data[i][2:-1]
+            table_data[i][1] = splited[0]
+            table_data[i][2] = splited[1]
+            wr.writerow(table_data[i])
+            i += 1
         f.close()
